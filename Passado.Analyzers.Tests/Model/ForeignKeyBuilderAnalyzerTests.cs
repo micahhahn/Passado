c38993f7ae2c19001d9d95322f3458a7b6d1de79
Micah Hahn <micah.s.hahn@gmail.com>
2017-08-10 19:28:42 -0500

    Add foreign key tests

---
+++ Passado.Analyzers.Tests/Model/ForeignKeyBuilderAnalyzerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+using Passado.Tests.Model;
+
+namespace Passado.Analyzers.Tests.Model
+{
+    public class ForeignKeyBuilderAnalyzerTests : ForeignKeyBuilderTests
+    {
+        public override Task<List<(string ErrorId, string ErrorText, Location Location, Location AdditionalLocation)>> GetErrorsFromCompilation(Compilation compilation) => AnalyzerHelpers.GetErrorsFromCompilation(compilation);
+    }
+}
---
+++ Passado.Tests/Model/ForeignKeyBuilderTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+using Xunit;
+
+namespace Passado.Tests.Model
+{
+    public abstract class ForeignKeyBuilderTests : ModelBuilderTests
+    {
+        public async Task VerifyForeignKeyErrorRaised(ModelBuilderError error, string location, string foreignKey)
+        {
+            var mb = @"var userId = 7;
+                       mb.Database(nameof(Database))
+                         .Table(d => d.Table(t => t.Users)
+                                      .Column(t => t.UserId, SqlType.Int)
+                                      .Column(t => t.UserType, SqlType.Int)                                
+                                      " + string.Format(foreignKey, location) + @"
+                                      .Build())
+                         .Build();";
+
+            await VerifyErrorRaised(mb, error, location);
+        }
+
+        [Fact]
+        public void Error_For_Everything()
+        {
+            Assert.True(false);
+        }
+    }
+
+    public class ForeignKeyBuilderCoreTests : ForeignKeyBuilderTests
+    {
+        public override Task<List<(string ErrorId, string ErrorText, Location Location, Location AdditionalLocation)>> GetErrorsFromCompilation(Compilation compilation) => CoreHelpers.GetErrorsFromCompilation(compilation);
+    }
+}
